c0b01d5f71a218a40be4a639cc4dcc8ba0f4809d
Laureano Gallegos <laureanogallegos@cetcom.com.ar>
2023-09-18 21:10:08 -0300

    IncompletoFix on Sql

---
+++ Controladora/ControladoraMedicamento.cs
@@ -0,0 +1,22 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controladora
+{
+    public class ControladoraMedicamento
+    {
+        public ControladoraMedicamento()
+        {
+
+        }
+
+        public string Agregar(Medicamento medicamento)
+        {
+
+        }
+    }
+}
---
+++ Modelo/RepositorioMedicamentos.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class RepositorioMedicamentos
+    {
+        private static RepositorioMedicamentos instancia;
+        private List<Medicamento> medicamentos;
+        private IConfigurationRoot configuration;
+
+        private RepositorioMedicamentos()
+        {
+            configuration = ConfigurationHelper.GetConfiguration("appSettings.json");
+            medicamentos = new List<Medicamento>();
+            RecuperarMedicamentos();
+        }
+
+        private RepositorioMedicamentos Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new RepositorioMedicamentos();
+                }
+                return instancia;
+            }
+        }
+
+        public bool Agregar(Medicamento medicamento)
+        {
+            if (AgregarMedicamento(medicamento))
+            {
+                medicamentos.Add(medicamento);
+                return true;
+            }
+            return false;
+        }
+
+        private bool AgregarMedicamento(Medicamento medicamento)
+        {
+            var ok = false;
+            var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+
+
+        }
+
+        public bool Eliminar(Medicamento medicamento)
+        {
+            if (EliminarMedicamento(medicamento))
+            {
+                medicamentos.Remove(medicamento);
+                return true;
+            }
+            return false;
+        }
+
+        private void RecuperarMedicamentos()
+        {
+            using (var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection")))
+
+                try
+                {
+                    using var command = new SqlCommand();
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.CommandText = "SP_RECUPERARMEDICAMENTOS";
+                    command.Connection = connection;
+                    command.Connection.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var medicamento = new Medicamento();
+                        medicamento.NombreComercial = reader["NOMBRE_COMERCIAL"].ToString();
+                        medicamento.EsVentaLibre = Convert.ToBoolean(reader["ES_VENTA_LIBRE"]);
+                        medicamento.PrecioVenta = Convert.ToDecimal(reader["PRECIO_VENTA"]);
+                        medicamento.Stock = Convert.ToInt32(reader["STOCK"]);
+                        medicamento.StockMinimo = Convert.ToInt32(reader["STOCK_MINIMO"]);
+
+                        medicamentos.Add(medicamento);
+                    }
+                    command.Connection.Close();
+
+                }
+                catch (SqlException ex)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+        }
+    }
+}
+
