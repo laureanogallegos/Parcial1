f9d88e185009f861c4beea2a2107fa5bc64a7ce1
ivon speranza <speranza.ivon@outlook.com>
2023-09-20 08:48:52 -0300

    SperanzaIvon2an

---
+++ Controladora/Controladora.cs
@@ -0,0 +1,51 @@
+﻿namespace Controladora
+{
+    public class Controladora
+    {
+
+
+        private ControladoraMonodroga() { }
+        private static ControladoraMonodroga instancia;
+        public static ControladoraMonodroga Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                    instancia = new ControladoraMonodroga();
+                return instancia;
+            }
+        }
+        public ReadOnlyCollection<Modelo.Monodroga> RecuperarCategoriasHabilitadas()
+        {
+            var listado = Modelo.RepositorioMonodrogas.Instancia.RepositorioMonodrogas();
+            return listado.Where(x => x.Habilitado == true).ToList().AsReadOnly();
+        }
+        public ReadOnlyCollection<Modelo.Monodroga> Recuperar()
+        {
+            try
+            {
+                return Modelo.RepositorioMonodrogas.Instancia.Monodrogas();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public string AgregarDroga(Monodroga monodroga)
+        {
+            try
+            {
+                var Monodroga = RepositorioMonodrogas.Instancia.Recuperar();
+                var drogaenccontrada = Monodroga.FirstOrDefault(x => x.Nombre.ToLower() == monodroga.Nombre.ToLower());
+                if (drogaenccontrada == null)
+                {
+                    var ok = RepositorioMonodrogas.Instancia.Agregar(monodroga);
+                    if (ok)
+                    {
+                        return $"La monodroga {monodroga.Nombre} se agregó correctamente";
+                    }
+                    else return $"La monodroga {monodroga.Nombre} no se ha podido agregar";
+                }
+            }
+    } } }
